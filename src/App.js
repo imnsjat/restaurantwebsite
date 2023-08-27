1a8ac77bd8bb6800b85b229a026a4dca050ad7fe
Naveen <sheoran19997@gmail.com>
2023-08-27 19:38:06 +0530

    added cart button function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import Header from "./Layout/Header";
-import Meals from "./Meals/Meals";
+import { useState } from 'react';
 
-
-
+import Header from './Layout/Header';
+import Meals from './Meals/Meals';
+import Cart from './Cart/Cart';
+import CartProvider from './Store/CartProovider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <>
-    <Header/>
-    <main><Meals/></main>
-      
-  
-    </>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
   );
 }
 
